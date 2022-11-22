2f52c95685fd5c1843be6edd789f721e2bd435d5
Alezuna Nadhif <nadhifalezuna@gmail.com>
2022-11-22 20:54:16 +0700

    portofolio

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// Get Navbar toggle Button
+const toggleButton = document.getElementsByClassName("toggle")[0];
+
+// Get Navbar item
+const navbarItems = document.getElementsByClassName("navbar-item");
+
+// event toggle button is clicked
+toggleButton.addEventListener("click", () => {
+    // transition navbar items
+    for (let i = 0; i <navbarItems.length; i++) {
+        navbarItems[i].classList.toggle("active")
+    }
+});
